53cc22e77c69626e91683ed0bc2b10a07e9e7b8b
defender-8 <kondratenko.ov.8@gmail.com>
2021-12-21 12:13:39 +0200

    Implemented getHitPower, getBlockPower, getDamage functions

--- src/javascript/components/fight.js
+++ src/javascript/components/fight.js
@@ -8,12 +8,25 @@ export async function fight(firstFighter, secondFighter) {
 
 export function getDamage(attacker, defender) {
   // return damage
+  return getHitPower(attacker) - getBlockPower(defender);
 }
 
 export function getHitPower(fighter) {
   // return hit power
+  const criticalHitChance = getRandomNumber(1, 2);
+  const power = fighter.attack * criticalHitChance;
+
+  return power;
 }
 
 export function getBlockPower(fighter) {
   // return block power
+  const dodgeChance = getRandomNumber(1, 2);
+  const power = fighter.defense * dodgeChance;
+
+  return power;
+}
+
+function getRandomNumber(min, max) {
+  return Math.random() * (max - min) + min;
 }
