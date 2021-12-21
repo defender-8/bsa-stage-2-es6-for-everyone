9ba4a60f4b6951522aed902c67fad9472f0f9bcf
defender-8 <kondratenko.ov.8@gmail.com>
2021-12-21 23:31:26 +0200

    Added fight functional. Added winner modal

--- src/javascript/components/fight.js
+++ src/javascript/components/fight.js
@@ -1,14 +1,75 @@
 import { controls } from '../../constants/controls';
 
-export async function fight(firstFighter, secondFighter) {
-  return new Promise((resolve) => {
-    // resolve the promise with the winner when fight is over
+export function fight(firstFighter, secondFighter) {
+  const firstFighterIndicator = document.getElementById('left-fighter-indicator');
+  const secondFighterIndicator = document.getElementById('right-fighter-indicator');
+  const indicatorContainerWidth = document.querySelector('.arena___health-indicator').offsetWidth;
+
+  let isFirstFighterInBlock = false;
+  let isSecondFighterInBlock = false;
+
+  document.addEventListener('keydown', (e) => {
+    if (e.code === controls.PlayerOneBlock) {
+      isFirstFighterInBlock = true;
+    }
+
+    if (e.code === controls.PlayerTwoBlock) {
+      isSecondFighterInBlock = true;
+    }
   });
+
+  return function (keyUpEvent) {
+    if (keyUpEvent.code === controls.PlayerOneBlock) {
+      isFirstFighterInBlock = false;
+    }
+
+    if (keyUpEvent.code === controls.PlayerTwoBlock) {
+      isSecondFighterInBlock = false;
+    }
+
+    if (keyUpEvent.code === controls.PlayerOneAttack) {
+      if (!isFirstFighterInBlock) {
+        const damage = isSecondFighterInBlock ? getDamage(firstFighter, secondFighter) : getHitPower(firstFighter);
+        const substrWidth = (indicatorContainerWidth * damage) / secondFighter.health;
+        const secondFighterIndicatorWidth =
+          ((secondFighterIndicator.offsetWidth - substrWidth) * 100) / indicatorContainerWidth;
+
+        secondFighterIndicator.style.width = secondFighterIndicatorWidth > 0 ? secondFighterIndicatorWidth + '%' : 0;
+
+        if (secondFighterIndicatorWidth <= 0) {
+          return new Promise((resolve) => {
+            // resolve the promise with the winner when fight is over
+            resolve(firstFighter);
+          });
+        }
+      }
+    }
+
+    if (keyUpEvent.code === controls.PlayerTwoAttack) {
+      if (!isSecondFighterInBlock) {
+        const damage = isFirstFighterInBlock ? getDamage(secondFighter, firstFighter) : getHitPower(secondFighter);
+        const substrWidth = (indicatorContainerWidth * damage) / firstFighter.health;
+        const firstFighterIndicatorWidth =
+          ((firstFighterIndicator.offsetWidth - substrWidth) * 100) / indicatorContainerWidth;
+
+        firstFighterIndicator.style.width = firstFighterIndicatorWidth > 0 ? firstFighterIndicatorWidth + '%' : 0;
+
+        if (firstFighterIndicatorWidth <= 0) {
+          return new Promise((resolve) => {
+            // resolve the promise with the winner when fight is over
+            resolve(secondFighter);
+          });
+        }
+      }
+    }
+  };
 }
 
 export function getDamage(attacker, defender) {
   // return damage
-  return getHitPower(attacker) - getBlockPower(defender);
+  const damage = getHitPower(attacker) - getBlockPower(defender);
+
+  return damage > 0 ? damage : 0;
 }
 
 export function getHitPower(fighter) {
--- src/javascript/components/modal/winner.js
+++ src/javascript/components/modal/winner.js
@@ -1,3 +1,10 @@
+import { showModal } from './modal';
+import { createFighterImage } from '../fighterPreview';
+
 export function showWinnerModal(fighter) {
-  // call showModal function 
+  const title = `${fighter?.name} is the winner!`;
+  const bodyElement = createFighterImage(fighter);
+  const onClose = () => window.location.reload();
+
+  showModal({ title, bodyElement, onClose });
 }
